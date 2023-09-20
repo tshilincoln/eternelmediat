9d62beaa12191a1f6b434491a65c334c41bc9384
tshilincoln <tshiabraham@gmail.com>
2023-09-20 11:27:23 +0200

    hr

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -1,6 +1,5 @@
-import { BsFillTelephoneOutboundFill } from "react-icons/bs";
-import { FaAmilia, FaGithub, FaGithubAlt, FaInstagramSquare, FaLinkedin, FaLinkedinIn, FaMailBulk, FaTwitterSquare, FaWhatsapp, FaWhatsappSquare } from "react-icons/fa";
-import { ImFacebook, ImFacebook2 } from "react-icons/im";
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { ImFacebook } from "react-icons/im";
 import { SiGmail } from "react-icons/si";
 
 
@@ -8,7 +7,7 @@ import { SiGmail } from "react-icons/si";
 export default function Copyright(){
     return(
         <>
-          <hr  className="absolute w-full text-center justify-center items-center"/>
+          <hr  className=" text-center flex items-center" />
           <section className='relative w-full h-14 md:h-28 pt-6 md:pt-12 px-6 md:px-10 flex justify-between text-sm py-1'>
             <p>Copyright © 2023 ClassDev</p>
             <div className="left-0 z-30 flex text-lg md:text-2xl md:ml-16 md:mb-16 gap-3">
